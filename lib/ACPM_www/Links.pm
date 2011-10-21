383749eca6e6249f231fee2d829e2c4c66b29cb0
Trevor Parker <trevor@trevorparker.com>
2011-10-20 20:28:46 -0400

    oops. we needed that other file.

---
+++ lib/ACPM_www/Links.pm
@@ -0,0 +1,16 @@
+package ACPM_www::Links;
+
+use strict;
+use warnings;
+
+sub links {
+	return [
+		[ 'Home',         '/' ],
+		[ 'Meetings',     '#' ],
+		[ 'People',       '#' ],
+		[ 'Twitter',      'http://twitter.com/#!/acperlmongers' ],
+		[ 'Mailing List', 'http://mail.pm.org/mailman/listinfo/atlanticcity-pm' ],
+	];
+}
+
+1;
